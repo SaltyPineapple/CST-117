79216a74e3bf02011a5350044c6f185103103ee6
Mark Pratt <32585200+SaltyPineapple@users.noreply.github.com>
2020-03-29 21:06:06 -0700

    Uploaded Project 1

---
+++ Project 1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            decimal num = decimal.Parse(dollarInput.Text);
+
+            decimal conversion = num * decimal.Parse("0.9");
+
+            Console.WriteLine(conversion);
+
+            euroOutput.Text = conversion.ToString("F3");
+        }
+    }
+}
