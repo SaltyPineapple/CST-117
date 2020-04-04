f1af09669c246ef09a1d75bc95d39855f1496a23
Mark Pratt <32585200+SaltyPineapple@users.noreply.github.com>
2020-04-04 16:59:44 -0700

    Uploaded exercise 4

---
+++ Exercise 4/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Exercise_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.secondsLabel = new System.Windows.Forms.Label();
+            this.secondsInput = new System.Windows.Forms.TextBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // secondsLabel
+            // 
+            this.secondsLabel.AutoSize = true;
+            this.secondsLabel.Location = new System.Drawing.Point(12, 70);
+            this.secondsLabel.Name = "secondsLabel";
+            this.secondsLabel.Size = new System.Drawing.Size(219, 20);
+            this.secondsLabel.TabIndex = 0;
+            this.secondsLabel.Text = "Enter the number of seconds:";
+            // 
+            // secondsInput
+            // 
+            this.secondsInput.Location = new System.Drawing.Point(238, 70);
+            this.secondsInput.Name = "secondsInput";
+            this.secondsInput.Size = new System.Drawing.Size(100, 26);
+            this.secondsInput.TabIndex = 1;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(12, 242);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(58, 20);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Text = "Output";
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(238, 236);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(100, 26);
+            this.outputBox.TabIndex = 3;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(155, 141);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(183, 46);
+            this.convertButton.TabIndex = 4;
+            this.convertButton.Text = "CONVERT";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 502);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.secondsInput);
+            this.Controls.Add(this.secondsLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label secondsLabel;
+        private System.Windows.Forms.TextBox secondsInput;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button convertButton;
+    }
+}
+
---
+++ Exercise 4/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Exercise_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                decimal seconds = decimal.Parse(secondsInput.Text);
+                decimal minutes = seconds / decimal.Parse("60.0");
+                decimal hours = minutes / decimal.Parse("60.0");
+                decimal days = hours / decimal.Parse("24.0");
+
+
+
+                if(seconds > 86400)
+                {
+                    outputBox.Text = days.ToString("F2");
+                    outputLabel.Text = "Number of Days: ";
+                }
+                else if(seconds > 3600)
+                {
+                    outputBox.Text = hours.ToString("F2");
+                    outputLabel.Text = "Number of Hours: ";
+                }
+                else if(seconds > 60)
+                {
+                    outputBox.Text = minutes.ToString("F2");
+                    outputLabel.Text = "Number of Minutes: ";
+                }
+                else
+                {
+                    outputBox.Text = seconds.ToString();
+                    outputLabel.Text = "Number of Seconds: ";
+                }
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Please make sure you enter a valid number. Decimals are OK");
+            }
+
+
+        }
+    }
+}
