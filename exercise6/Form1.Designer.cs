cd140cbc6a9d33648f5678ed98fb4d7d74c747df
Mark Pratt <32585200+SaltyPineapple@users.noreply.github.com>
2020-04-19 14:11:05 -0700

    Uploaded Exercise 6

---
+++ exercise6/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace exercise6
+{
+    partial class exercise6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.diceRollButton = new System.Windows.Forms.Button();
+            this.diceOneLabel = new System.Windows.Forms.Label();
+            this.diceTwoLabel = new System.Windows.Forms.Label();
+            this.diceSidesLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // diceRollButton
+            // 
+            this.diceRollButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.diceRollButton.Location = new System.Drawing.Point(12, 12);
+            this.diceRollButton.Name = "diceRollButton";
+            this.diceRollButton.Size = new System.Drawing.Size(152, 115);
+            this.diceRollButton.TabIndex = 0;
+            this.diceRollButton.Text = "Roll Dice";
+            this.diceRollButton.UseVisualStyleBackColor = true;
+            this.diceRollButton.Click += new System.EventHandler(this.diceRollButton_Click);
+            // 
+            // diceOneLabel
+            // 
+            this.diceOneLabel.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.diceOneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.diceOneLabel.Location = new System.Drawing.Point(190, 12);
+            this.diceOneLabel.Name = "diceOneLabel";
+            this.diceOneLabel.Size = new System.Drawing.Size(169, 115);
+            this.diceOneLabel.TabIndex = 1;
+            this.diceOneLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // diceTwoLabel
+            // 
+            this.diceTwoLabel.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.diceTwoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.diceTwoLabel.Location = new System.Drawing.Point(380, 12);
+            this.diceTwoLabel.Name = "diceTwoLabel";
+            this.diceTwoLabel.Size = new System.Drawing.Size(170, 115);
+            this.diceTwoLabel.TabIndex = 2;
+            this.diceTwoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // diceSidesLabel
+            // 
+            this.diceSidesLabel.AutoSize = true;
+            this.diceSidesLabel.Location = new System.Drawing.Point(12, 135);
+            this.diceSidesLabel.Name = "diceSidesLabel";
+            this.diceSidesLabel.Size = new System.Drawing.Size(0, 20);
+            this.diceSidesLabel.TabIndex = 3;
+            // 
+            // exercise6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 164);
+            this.Controls.Add(this.diceSidesLabel);
+            this.Controls.Add(this.diceTwoLabel);
+            this.Controls.Add(this.diceOneLabel);
+            this.Controls.Add(this.diceRollButton);
+            this.Name = "exercise6";
+            this.Text = "Dice Roll";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button diceRollButton;
+        private System.Windows.Forms.Label diceOneLabel;
+        private System.Windows.Forms.Label diceTwoLabel;
+        private System.Windows.Forms.Label diceSidesLabel;
+    }
+}
+
---
+++ exercise6/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exercise6
+{
+    public partial class exercise6 : Form
+    {
+        public exercise6()
+        {
+            InitializeComponent();
+
+        }
+
+        /* Create instance of random
+         * Create two dice objects
+         * Instantiate roll counter
+         */
+        static Random random = new Random();
+        static int num = random.Next(4, 21);
+        Dice diceOne = new Dice(num);
+        Dice diceTwo = new Dice(num);
+        int rollCount = 0;
+
+       
+
+        private void diceRollButton_Click(object sender, EventArgs e)
+        {
+            // Set label showing how many sides dice have
+            diceSidesLabel.Text = String.Format("The dice have {0} sides", num);
+            
+            // Create stop case for recursion
+            Boolean diceOne1 = false;
+            Boolean diceTwo1 = false;
+         
+            if(!diceOne1 && !diceTwo1)
+            {
+                // incrementer
+                rollCount++;
+
+                // Roll both dice. Uses the same instance of random in order to maintain randomness
+                int diceRollOne = diceOne.rollDice(random.Next(1, diceOne.getDiceSides() + 1));
+                int diceRollTwo = diceTwo.rollDice(random.Next(1, diceTwo.getDiceSides() + 1));
+
+                // Document each roll
+                Console.WriteLine(String.Format("Roll #:{0}", rollCount));
+                Console.WriteLine(String.Format("Dice One: {0}", diceRollOne));
+                Console.WriteLine(String.Format("Dice Two: {0}", diceRollTwo));
+                Console.WriteLine(" ");
+
+                // Show roll on dice in UI
+                diceOneLabel.Text = diceRollOne.ToString();
+                diceTwoLabel.Text = diceRollTwo.ToString();         
+
+                // If snake eyes are rolled, show how many rolls it took
+                // reset roll counter
+                // otherwise recursively run again
+                if (diceRollOne == 1 && diceRollTwo == 1)
+                {
+                    diceOne1 = true;
+                    diceTwo1 = true;
+                    MessageBox.Show(String.Format("It took {0} rolls to get snake eyes!", rollCount));
+                    rollCount = 0;
+                }
+                else
+                {
+                    diceRollButton_Click(sender, e);
+                }
+            }
+            
+        }
+
+       
+    }
+
+    /* Dice Class
+     * Create dice with given sides
+     * Roll dice and return random number
+     */
+    class Dice
+    {
+        private int diceSides;
+        private Random random = new Random();
+        
+
+        public Dice(int sides)
+        {
+            diceSides = sides;
+            Console.WriteLine(sides);
+            
+        }
+
+        public int getDiceSides()
+        {
+            return diceSides;
+        }
+
+        public int rollDice(int num)
+        {
+            return num;
+        }
+    }
+
+}
