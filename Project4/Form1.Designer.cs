75bea8a206b0258ac8d9ff4a78f308e24c068958
Mark Pratt <32585200+SaltyPineapple@users.noreply.github.com>
2020-05-03 18:20:47 -0700

    Uploaded Project 4

---
+++ Project4/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Project4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.square1 = new System.Windows.Forms.Label();
+            this.square2 = new System.Windows.Forms.Label();
+            this.square3 = new System.Windows.Forms.Label();
+            this.square4 = new System.Windows.Forms.Label();
+            this.square5 = new System.Windows.Forms.Label();
+            this.square6 = new System.Windows.Forms.Label();
+            this.square7 = new System.Windows.Forms.Label();
+            this.square8 = new System.Windows.Forms.Label();
+            this.square9 = new System.Windows.Forms.Label();
+            this.playButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // square1
+            // 
+            this.square1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.square1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.square1.Location = new System.Drawing.Point(36, 40);
+            this.square1.Name = "square1";
+            this.square1.Size = new System.Drawing.Size(94, 93);
+            this.square1.TabIndex = 2;
+            this.square1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.square1.Click += new System.EventHandler(this.square1_Click);
+            // 
+            // square2
+            // 
+            this.square2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.square2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.square2.Location = new System.Drawing.Point(136, 40);
+            this.square2.Name = "square2";
+            this.square2.Size = new System.Drawing.Size(94, 93);
+            this.square2.TabIndex = 3;
+            this.square2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.square2.Click += new System.EventHandler(this.square2_Click);
+            // 
+            // square3
+            // 
+            this.square3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.square3.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.square3.Location = new System.Drawing.Point(236, 40);
+            this.square3.Name = "square3";
+            this.square3.Size = new System.Drawing.Size(94, 93);
+            this.square3.TabIndex = 4;
+            this.square3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.square3.Click += new System.EventHandler(this.square3_Click);
+            // 
+            // square4
+            // 
+            this.square4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.square4.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.square4.Location = new System.Drawing.Point(36, 133);
+            this.square4.Name = "square4";
+            this.square4.Size = new System.Drawing.Size(94, 93);
+            this.square4.TabIndex = 5;
+            this.square4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.square4.Click += new System.EventHandler(this.square4_Click);
+            // 
+            // square5
+            // 
+            this.square5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.square5.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.square5.Location = new System.Drawing.Point(136, 133);
+            this.square5.Name = "square5";
+            this.square5.Size = new System.Drawing.Size(94, 93);
+            this.square5.TabIndex = 6;
+            this.square5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.square5.Click += new System.EventHandler(this.square5_Click);
+            // 
+            // square6
+            // 
+            this.square6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.square6.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.square6.Location = new System.Drawing.Point(236, 133);
+            this.square6.Name = "square6";
+            this.square6.Size = new System.Drawing.Size(94, 93);
+            this.square6.TabIndex = 7;
+            this.square6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.square6.Click += new System.EventHandler(this.square6_Click);
+            // 
+            // square7
+            // 
+            this.square7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.square7.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.square7.Location = new System.Drawing.Point(36, 226);
+            this.square7.Name = "square7";
+            this.square7.Size = new System.Drawing.Size(94, 93);
+            this.square7.TabIndex = 8;
+            this.square7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.square7.Click += new System.EventHandler(this.square7_Click);
+            // 
+            // square8
+            // 
+            this.square8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.square8.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.square8.Location = new System.Drawing.Point(136, 226);
+            this.square8.Name = "square8";
+            this.square8.Size = new System.Drawing.Size(94, 93);
+            this.square8.TabIndex = 9;
+            this.square8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.square8.Click += new System.EventHandler(this.square8_Click);
+            // 
+            // square9
+            // 
+            this.square9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.square9.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.square9.Location = new System.Drawing.Point(236, 226);
+            this.square9.Name = "square9";
+            this.square9.Size = new System.Drawing.Size(94, 93);
+            this.square9.TabIndex = 10;
+            this.square9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.square9.Click += new System.EventHandler(this.square9_Click);
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(36, 383);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(125, 47);
+            this.playButton.TabIndex = 11;
+            this.playButton.Text = "Play Again";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(205, 383);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(125, 47);
+            this.exitButton.TabIndex = 12;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 468);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.square9);
+            this.Controls.Add(this.square8);
+            this.Controls.Add(this.square7);
+            this.Controls.Add(this.square6);
+            this.Controls.Add(this.square5);
+            this.Controls.Add(this.square4);
+            this.Controls.Add(this.square3);
+            this.Controls.Add(this.square2);
+            this.Controls.Add(this.square1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label square1;
+        private System.Windows.Forms.Label square2;
+        private System.Windows.Forms.Label square3;
+        private System.Windows.Forms.Label square4;
+        private System.Windows.Forms.Label square5;
+        private System.Windows.Forms.Label square6;
+        private System.Windows.Forms.Label square7;
+        private System.Windows.Forms.Label square8;
+        private System.Windows.Forms.Label square9;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Project4/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project4
+{
+    public partial class Form1 : Form
+    {
+        
+        int turnCounter = 0;
+        int boardCounter = 0;
+        
+        int[,] board = new int[3, 3];
+
+
+        Boolean isClickedSquare1 = false;
+        Boolean isClickedSquare2 = false;
+        Boolean isClickedSquare3 = false;
+        Boolean isClickedSquare4 = false;
+        Boolean isClickedSquare5 = false;
+        Boolean isClickedSquare6 = false;
+        Boolean isClickedSquare7 = false;
+        Boolean isClickedSquare8 = false;
+        Boolean isClickedSquare9 = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void square1_Click(object sender, EventArgs e)
+        {
+            if(!isClickedSquare1){
+                fillSquare(square1, 0, 0);
+                isClickedSquare1 = true;
+            }
+
+        }
+
+        private void square2_Click(object sender, EventArgs e)
+        {
+            if(!isClickedSquare2){
+                fillSquare(square2, 0, 1);
+                isClickedSquare2 = true;
+            }
+            
+        }
+
+        private void square3_Click(object sender, EventArgs e)
+        {
+            if(!isClickedSquare3){
+                fillSquare(square3, 0, 2);
+                isClickedSquare3 = true;
+            }
+            
+        }
+
+        private void square4_Click(object sender, EventArgs e)
+        {
+            if(!isClickedSquare4){
+                fillSquare(square4, 1, 0);
+                isClickedSquare4 = true;
+            }
+            
+        }
+
+        private void square5_Click(object sender, EventArgs e)
+        {
+            if(!isClickedSquare5){
+                fillSquare(square5, 1, 1);
+                isClickedSquare5 = true;
+            }
+            
+        }
+
+        private void square6_Click(object sender, EventArgs e)
+        {
+            if(!isClickedSquare6){
+                fillSquare(square6, 1, 2);
+                isClickedSquare6 = true;
+            }
+            
+        }
+
+        private void square7_Click(object sender, EventArgs e)
+        {
+            if(!isClickedSquare7){
+                fillSquare(square7, 2, 0);
+                isClickedSquare7 = true;
+            }
+            
+        }
+
+        private void square8_Click(object sender, EventArgs e)
+        {
+            if (!isClickedSquare8) {
+                fillSquare(square8, 2, 1);
+                isClickedSquare8 = true;
+            }
+            
+        }
+
+        private void square9_Click(object sender, EventArgs e)
+        {
+            if (!isClickedSquare9) {
+                fillSquare(square9, 2, 2);
+                isClickedSquare9 = true;
+            }
+            
+        }
+
+
+
+        public void fillSquare(Label label, int x, int y){
+           
+            if (turnCounter % 2 == 0)
+            {
+                label.Text = "X";
+                board[x, y] = 1;
+                checkWin(1,"X");
+            }
+            else
+            {
+                label.Text = "O";
+                board[x, y] = 2;
+                checkWin(2,"O");
+            }
+
+            printBoard();
+
+            
+            turnCounter++;
+
+        }
+
+        public void checkWin(int playerNum, string playerMarker) {
+
+            
+
+            // Check Horizontally
+            if(board[0,0] == playerNum && board[0,1] == playerNum && board[0,2] == playerNum) {
+                MessageBox.Show(string.Format("Player {0} Wins!", playerMarker));
+
+                isClickedSquare1 = true;
+                isClickedSquare2 = true;
+                isClickedSquare3 = true;
+                isClickedSquare4 = true;
+                isClickedSquare5 = true;
+                isClickedSquare6 = true;
+                isClickedSquare7 = true;
+                isClickedSquare8 = true;
+                isClickedSquare9 = true;
+
+            }
+            else if (board[1, 0] == playerNum && board[1, 1] == playerNum && board[1, 2] == playerNum) {
+                MessageBox.Show(string.Format("Player {0} Wins!", playerMarker));
+
+                isClickedSquare1 = true;
+                isClickedSquare2 = true;
+                isClickedSquare3 = true;
+                isClickedSquare4 = true;
+                isClickedSquare5 = true;
+                isClickedSquare6 = true;
+                isClickedSquare7 = true;
+                isClickedSquare8 = true;
+                isClickedSquare9 = true;
+
+            }
+            else if (board[2, 0] == playerNum && board[2, 1] == playerNum && board[2, 2] == playerNum) {
+                MessageBox.Show(string.Format("Player {0} Wins!", playerMarker));
+
+                isClickedSquare1 = true;
+                isClickedSquare2 = true;
+                isClickedSquare3 = true;
+                isClickedSquare4 = true;
+                isClickedSquare5 = true;
+                isClickedSquare6 = true;
+                isClickedSquare7 = true;
+                isClickedSquare8 = true;
+                isClickedSquare9 = true;
+
+            }
+
+            //Check Vertically
+            else if (board[0, 0] == playerNum && board[1, 0] == playerNum && board[2, 0] == playerNum) {
+                MessageBox.Show(string.Format("Player {0} Wins!", playerMarker));
+
+                isClickedSquare1 = true;
+                isClickedSquare2 = true;
+                isClickedSquare3 = true;
+                isClickedSquare4 = true;
+                isClickedSquare5 = true;
+                isClickedSquare6 = true;
+                isClickedSquare7 = true;
+                isClickedSquare8 = true;
+                isClickedSquare9 = true;
+
+            }
+            else if (board[0, 1] == playerNum && board[1, 1] == playerNum && board[2, 1] == playerNum) {
+                MessageBox.Show(string.Format("Player {0} Wins!", playerMarker));
+
+                isClickedSquare1 = true;
+                isClickedSquare2 = true;
+                isClickedSquare3 = true;
+                isClickedSquare4 = true;
+                isClickedSquare5 = true;
+                isClickedSquare6 = true;
+                isClickedSquare7 = true;
+                isClickedSquare8 = true;
+                isClickedSquare9 = true;
+
+            }
+            else if (board[0, 2] == playerNum && board[1, 2] == playerNum && board[2, 2] == playerNum) {
+                MessageBox.Show(string.Format("Player {0} Wins!", playerMarker));
+
+                isClickedSquare1 = true;
+                isClickedSquare2 = true;
+                isClickedSquare3 = true;
+                isClickedSquare4 = true;
+                isClickedSquare5 = true;
+                isClickedSquare6 = true;
+                isClickedSquare7 = true;
+                isClickedSquare8 = true;
+                isClickedSquare9 = true;
+
+            }
+
+            //Check Diagonal
+            else if (board[0, 0] == playerNum && board[1, 1] == playerNum && board[2, 2] == playerNum) {
+                MessageBox.Show(string.Format("Player {0} Wins!", playerMarker));
+
+                isClickedSquare1 = true;
+                isClickedSquare2 = true;
+                isClickedSquare3 = true;
+                isClickedSquare4 = true;
+                isClickedSquare5 = true;
+                isClickedSquare6 = true;
+                isClickedSquare7 = true;
+                isClickedSquare8 = true;
+                isClickedSquare9 = true;
+
+            }
+            else if (board[2, 0] == playerNum && board[1, 1] == playerNum && board[0, 2] == playerNum) {
+                MessageBox.Show(string.Format("Player {0} Wins!", playerMarker));
+
+                isClickedSquare1 = true;
+                isClickedSquare2 = true;
+                isClickedSquare3 = true;
+                isClickedSquare4 = true;
+                isClickedSquare5 = true;
+                isClickedSquare6 = true;
+                isClickedSquare7 = true;
+                isClickedSquare8 = true;
+                isClickedSquare9 = true;
+
+            }
+            else {
+                if(turnCounter == 8) {
+                    MessageBox.Show("Cats Game!");
+                }
+                
+            }
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void playButton_Click(object sender, EventArgs e) {
+            for(int x=0; x<3; x++) {
+                for(int y=0; y<3; y++) {
+                    board[x, y] = 0;
+                }
+            }
+
+            turnCounter = 0;
+
+            square1.Text = " ";
+            isClickedSquare1 = false;
+
+            square2.Text = " ";
+            isClickedSquare2 = false;
+
+            square3.Text = " ";
+            isClickedSquare3 = false;
+
+            square4.Text = " ";
+            isClickedSquare4 = false;
+
+            square5.Text = " ";
+            isClickedSquare5 = false;
+
+            square6.Text = " ";
+            isClickedSquare6 = false;
+
+            square7.Text = " ";
+            isClickedSquare7 = false;
+
+            square8.Text = " ";
+            isClickedSquare8 = false;
+
+            square9.Text = " ";
+            isClickedSquare9 = false;
+
+        }
+
+        public void printBoard() {
+            Console.WriteLine(string.Format("{0}, {1}, {2}", board[0, 0], board[0, 1], board[0, 2]));
+            Console.WriteLine(string.Format("{0}, {1}, {2}", board[1, 0], board[1, 1], board[1, 2]));
+            Console.WriteLine(string.Format("{0}, {1}, {2}", board[2, 0], board[2, 1], board[2, 2]));
+            Console.WriteLine(string.Format("TurnCounter: {0}", turnCounter));
+        }
+    }
+}
